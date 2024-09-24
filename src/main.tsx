d86da63854466f4a9bb5a28267527c16692144a1
PoPodada <goito.722@icloud.com>
2024-09-24 18:45:51 +0900

    ゲームスクリーンコンポーネントを追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import App from "./App";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Home</div>,
+    element: <App />,
   },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <div className="w-[700px] mx-auto">
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>
 );
