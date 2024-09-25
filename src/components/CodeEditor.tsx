3430bc1e8b821b1415535526b5dcc2dd6de0671c
53-gm <h20096@e.hiroshima-cu.ac.jp>
2024-09-26 01:27:12 +0900

    codeEditor追加

---
+++ src/components/CodeEditor.tsx
@@ -0,0 +1,30 @@
+import { useRef, useEffect, useState } from "react";
+import Editor from "@monaco-editor/react";
+import * as monacoEditor from "monaco-editor"; // monaco-editorの型をインポート
+
+
+interface CodeEditorProps {
+  code: string;
+  onChange: (value: string) => void;
+}
+
+
+const CodeEditor : React.FC<CodeEditorProps> = ({code, onChange}) => {
+  return (
+      <Editor
+        height="600px"
+        defaultLanguage="javascript"
+        defaultValue="// your code here"
+        value={code}
+        onChange={(value) => onChange(value || '')}
+        theme="vs-dark"
+        options={{
+                  minimap: { enabled: false },
+                  fontSize: 18,
+                  automaticLayout: true,
+                }}
+      />
+  );
+};
+
+export default CodeEditor;
