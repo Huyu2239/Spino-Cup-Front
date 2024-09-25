ab32f5fbfd9dd1a6e09390b63f38af5f5bf5bdad
PoPodada <goito.722@icloud.com>
2024-09-25 23:37:48 +0900

    問題文のastを表示

--- src/components/GameScreen.tsx
+++ src/components/GameScreen.tsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState, useRef } from "react";
 import { Button, TextField } from "@mui/material";
 import { getQuizzes, getResult, Quizzes } from "../api";
 import useCodeExecutor from "../hooks/useCodeExecutor";
-
+import ReactDiffViewer from "react-diff-viewer-continued";
+import { parse } from "acorn";
 type Position = {
   x: number;
   y: number;
@@ -15,21 +16,49 @@ const GameScreen = () => {
   });
   const [clickPosition, setClickPosition] = useState<Position>({ x: 0, y: 0 });
   const [quizzes, setQuizzes] = useState<Quizzes | undefined>(undefined);
+
   const [result, setResult] = useState<string>("");
   const gameAreaRef = useRef<HTMLDivElement>(null);
   const [textField, setTextField] = useState<string>("");
-  const handleText = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setTextField(event.target.value);
-  };
 
   const { executeCode, executionResult, clearOutput } = useCodeExecutor();
 
   const quiz = quizzes ? quizzes[0] : undefined;
+  const handleText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTextField(event.target.value);
+    const cloneQuestion = [...structuredClone(quiz!).question];
+    cloneQuestion[clickPosition.x][clickPosition.y] = event.target.value;
+    const newUserCode = cloneQuestion.flat().join(" ");
+    console.log("newUserCode", event.target.value);
+    setUserCode(newUserCode);
+  };
+  const [userCode, setUserCode] = useState<string>(
+    quiz ? quiz.question.flat().join(" ") : ""
+  );
+  let answer;
+  let ast = "";
+  let answerAst = "";
+  if (quiz) {
+    answer = quiz.question
+      .flat()
+      .map((word) =>
+        word === "console.log(evenNumbers.lenght);"
+          ? "console.log(evenNumbers.length);"
+          : word
+      )
+      .join(" ");
+    ast = JSON.stringify(parse(userCode), null, 2);
+    answerAst = JSON.stringify(parse(answer), null, 2);
+  }
 
   useEffect(() => {
     (async () => {
       const response = await getQuizzes();
       setQuizzes(response);
+      if (response) {
+        console.log("response", response[0].question.flat().join(" "));
+        setUserCode(response[0].question.flat().join(" "));
+      }
     })();
   }, []);
 
@@ -51,11 +80,15 @@ const GameScreen = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (!quiz) return;
+    executeCode(quiz.question.flat().join(" "));
+  }, []);
+
   const answerQuestion = async () => {
     if (!quiz) return;
     executeCode(quiz.question.flat().join(" "));
-    console.log(quiz.question.flat().join(" "));
-    console.log(executionResult.logs);
+
     const response = await getResult(
       quiz.id.toString(),
       clickPosition.y,
@@ -63,7 +96,10 @@ const GameScreen = () => {
       textField
     );
     if (response === undefined) return;
-    setResult(response.is_correct ? "正解" : "不正解");
+    // setResult(response.is_correct ? "正解" : "不正解");
+    console.log(ast === answerAst);
+
+    setResult(ast === answerAst ? "正解" : "不正解");
   };
 
   const onClickArea = (x: number, y: number, event: React.MouseEvent) => {
@@ -74,74 +110,99 @@ const GameScreen = () => {
   };
 
   return (
-    <div className=" p-4 font-mono">
-      <div
-        id="game-screen"
-        ref={gameAreaRef}
-        className="focus:outline-none text-2xl bg-gray-800 text-white p-4 mt-[50px] relative overflow-hidden min-h-[500px] w-full"
-      >
-        {quiz &&
-          quiz.question.map((row, i) => (
-            <div key={i} className="flex mb-2">
-              {row.map((cell, j) => (
-                <div
-                  key={j}
-                  className="mr-4 hover:bg-blue-500"
-                  onClick={(event) => onClickArea(i, j, event)}
-                >
-                  {cell === "" ? " " : cell}
-                </div>
-              ))}
-            </div>
-          ))}
-        <svg
-          id="cursor-overlay"
-          width="100%"
-          height="100%"
-          style={{
-            position: "absolute",
-            top: 0,
-            left: 0,
-            pointerEvents: "none",
-          }}
-        >
-          <defs>
-            <mask id="hole">
-              <rect width="100%" height="100%" fill="white" />
-              <ellipse
-                cx={cursorPosition.x}
-                cy={cursorPosition.y}
-                rx="120"
-                ry="30"
-                fill="black"
-              />
-            </mask>
-          </defs>
-          <rect width="100%" height="100%" fill="black" mask="url(#hole)" />
-        </svg>
-      </div>
-
-      <div className="mt-8 space-y-4">
+    <div className="flex max-h-screen">
+      {/* {ast && <pre>{ast}</pre>} */}
+      <div className="font-mono w-[50%] mx-2">
         <div className=" p-4 rounded-md border border-[#3c3c3c]">
-          <h3 className="text-xl font-semibold mb-2 ">選択している単語</h3>
-          <TextField
-            type="text"
-            value={textField}
-            onChange={handleText}
-            disabled={!textField}
-            sx={{ width: "100%" }}
-          />
+          <h3 className="text-xl font-semibold mb-2 ">エラー内容</h3>
+          <p></p>
         </div>
-        <Button onClick={answerQuestion} className="w-full" variant="contained">
-          回答
-        </Button>
-        <div className="p-4 rounded-md border border-[#3c3c3c] h-[100px]">
-          <h3 className="text-xl font-semibold mb-2">結果</h3>
-          <p className="text-xl font-bold">{result}</p>
+        <div
+          id="game-screen"
+          ref={gameAreaRef}
+          className="focus:outline-none text-2xl bg-gray-800 text-white p-4 mt-4 relative overflow-hidden min-h-[500px] w-full"
+        >
+          {quiz &&
+            quiz.question.map((row, i) => (
+              <div key={i} className="flex mb-2">
+                {row.map((cell, j) => (
+                  <div
+                    key={j}
+                    className="mr-4 hover:bg-blue-500"
+                    onClick={(event) => onClickArea(i, j, event)}
+                  >
+                    {cell === "" ? " " : cell}
+                  </div>
+                ))}
+              </div>
+            ))}
+
+          <svg
+            id="cursor-overlay"
+            width="100%"
+            height="100%"
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              pointerEvents: "none",
+            }}
+          >
+            <defs>
+              <mask id="hole">
+                <rect width="100%" height="100%" fill="white" />
+                <ellipse
+                  cx={cursorPosition.x}
+                  cy={cursorPosition.y}
+                  rx="120"
+                  ry="30"
+                  fill="black"
+                />
+              </mask>
+            </defs>
+            <rect width="100%" height="100%" fill="black" mask="url(#hole)" />
+          </svg>
         </div>
 
-        <p>{String(executionResult.logs)}</p>
+        <div className="mt-2 space-y-4">
+          <div className=" p-4 rounded-md border border-[#3c3c3c]">
+            <h3 className="text-xl font-semibold mb-2 ">選択している単語</h3>
+            <TextField
+              type="text"
+              value={textField}
+              onChange={handleText}
+              disabled={!textField}
+              sx={{ width: "100%" }}
+            />
+          </div>
+          <Button
+            onClick={answerQuestion}
+            className="w-full"
+            variant="contained"
+          >
+            回答
+          </Button>
+          <div className="p-4 rounded-md border border-[#3c3c3c] h-[100px]">
+            <h3 className="text-xl font-semibold mb-2">結果</h3>
+            <p className="text-xl font-bold">{result}</p>
+          </div>
+
+          {/* <p>{String(executionResult.logs)}</p> */}
+        </div>
       </div>
+      <div className="w-[50%] overflow-y-scroll">
+        <div className="flex h-[30px] bg-gray-400 sticky top-0 z-10 font-bold text-xl">
+          <p className="w-[50%]">問題</p>
+          <p className="w-[50%]">答え</p>
+        </div>
+        <ReactDiffViewer
+          oldValue={ast}
+          newValue={answerAst}
+          splitView={true}
+          showDiffOnly={false}
+        />
+      </div>
+      {/* {answerAst && <pre>{answerAst}</pre>} */}
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -18,8 +18,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div className="w-[800px] mx-auto">
-      <RouterProvider router={router} />
-    </div>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
